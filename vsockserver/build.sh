be68a2aea1709545987c903a851661a1cd733796
dahuahua <38651850@qq.com>
2022-09-30 15:35:37 +0800

    test c version jni

--- vsockserver/build.sh
+++ vsockserver/build.sh
@@ -15,4 +15,4 @@ docker rmi vsockserver:latest
 docker build -t vsockserver:latest .
 nitro-cli build-enclave --docker-uri vsockserver:latest  --output-file vsockserver.eif > EnclaveImage.log
 nitro-cli run-enclave --cpu-count 4 --memory 4096 --enclave-cid 5 --eif-path vsockserver.eif --debug-mode
-# nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
+nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
